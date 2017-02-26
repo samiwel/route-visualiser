764f95f20d19cd584185d808fe97a8d0531869a6
samiwel <samiwel.thomas@gmail.com>
2017-02-26 21:20:26 +0000

    Refactor code.

--- routeviz.py
+++ routeviz.py
@@ -9,20 +9,18 @@ class Block:
         self.name = name
         self.routing_rules = None
 
-def main(args):
+def read_json(filename=None):
+    if not filename:
+        raise NameError('JSON input file name not supplied.')
 
-    input_filename = args[1] if len(args) > 1 else 'data.json'
-    output_filename = args[2] if len(args) > 2 else 'output'
-    output_format = 'svg' # This might change later
-
-    schema_json = None
-    with open(file=input_filename, mode='r') as f:
-        schema_json = json.load(f)
+    json_content = None
+    with open(file=filename, mode='r') as f:
+        json_content = json.load(f)
         f.close()
 
-    if not schema_json:
-        print('ERROR could not read file')
+    return json_content
 
+def parse_schema_for_blocks(schema_json):
     blocks = []
     for group in schema_json['groups']:
         for block in group['blocks']:
@@ -30,7 +28,9 @@ def main(args):
             if 'routing_rules' in block:
                 b.routing_rules = block['routing_rules']
             blocks.append(b)
+    return blocks
 
+def construct_graph(output_format, blocks):
     graph = gv.Digraph(format=output_format)
     for index, block in enumerate(blocks):
         graph.node(block.name)
@@ -51,12 +51,24 @@ def main(args):
                     text = ' '.join(label_parts)
                     if len(text.strip()) > 0:
                         label = text
-
                 graph.edge(block.name, routing_rule['goto']['id'], label=label)
-
         elif index < len(blocks) - 1:
             graph.edge(blocks[index].name, blocks[index+1].name, label='Implicit')
 
+    return graph
+
+def main(args):
+    input_filename = args[1] if len(args) > 1 else 'data.json'
+    output_filename = args[2] if len(args) > 2 else 'output'
+    output_format = 'svg'
+
+    schema_json = read_json(input_filename)
+    if not schema_json:
+        print('Could not read JSON from', input_filename)
+        sys.exit(1)
+
+    blocks = parse_schema_for_blocks(schema_json)
+    graph = construct_graph(output_format, blocks)
     filename = graph.render(filename=output_filename, cleanup=True)
     print('Generated file', filename)
 
